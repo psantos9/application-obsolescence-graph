17dbda91fcd2603d181e6dc7d30e24bbbeec32d9
Jesus Bautista Lopez <jesusjblopez@gmail.com>
2023-11-23 18:40:05 -0500

    Add UI changes

--- src/components/VGraph.vue
+++ src/components/VGraph.vue
@@ -1,5 +1,10 @@
 <template>
   <v-network-graph ref="graph" :nodes="nodes" :edges="edges" :configs="configs" :eventHandlers="eventHandlers">
+    <template #override-node-label="{ scale, text }">
+      <text x="0" y="0" :font-size="8 * scale" text-anchor="middle" dominant-baseline="central" fill="#ffffff">
+        {{ text }}
+      </text>
+    </template>
     <template #edge-label="{ edge, ...slotProps }">
       <v-edge-label :text="edge.obsolescenceRiskStatus" align="center" vertical-align="above" v-bind="slotProps" />
     </template>
@@ -11,11 +16,18 @@ import { ref, computed, unref } from 'vue'
 import { defineConfigs, type VNetworkGraphInstance, type LayoutHandler, type Node } from 'v-network-graph'
 import { ForceLayout } from 'v-network-graph/lib/force-layout'
 import { useApi } from '@/composables/useApi'
+import dagre from '@dagrejs/dagre'
 
 const { graph: dataset, openFactSheetSidePane } = useApi()
 const layoutHandler: LayoutHandler = new ForceLayout()
 
 const graph = ref<VNetworkGraphInstance | null>(null)
+
+// Make references for tooltip, it's opacity, and the targetNode
+const tooltip = ref<HTMLDivElement>()
+const tooltipOpacity = ref(0)
+const targetNodeId = ref<string>('')
+const tooltipPos = ref({ left: '0px', top: '0px' })
 
 const nodes = computed(() => unref(dataset)?.nodes ?? {})
 const edges = computed(() => unref(dataset)?.edges ?? {})
@@ -33,6 +45,8 @@ const getNodeLabel = (node: Node): string => {
     children,
     requires
   } = node
+
+  // Not sure if I'll need this
   const label: string[] = [name]
   if (type === 'ITComponent') {
     if (missingLifecycle) label.push('missingLifecycle')
@@ -40,13 +54,19 @@ const getNodeLabel = (node: Node): string => {
       if (children.length + requires.length > 0) label.push(`aggregatedLifecycle: ${aggregatedLifecycleKey}`)
       label.push(`lifecycle: ${lifecycleKey}`)
       if (eol) label.push(`eol: ${eol}`)
-      else if (phaseOut) label.push(`phaseOut: ${phaseOut}`)
+      else if (phaseOut) label.push(`phaseOut: ${aggregatedLifecycleKey}`)
     }
   } else if (type === 'Application') {
     label.push(aggregatedObsolescenceRiskKey)
   }
   return label.join('\n')
 }
+
+// For icons later on
+const icons = {
+  accepted: '&#xe5ca',
+  addressed: '&#xe160'
+} as Record<string, string>
 
 const configs = defineConfigs({
   view: {
@@ -55,18 +75,34 @@ const configs = defineConfigs({
   },
   node: {
     normal: {
-      type: ({ type }) => (type === 'Application' ? 'circle' : 'circle'),
-      color: ({ type }) => (type === 'Application' ? '#4466cc' : '#ffe50d')
+      // If it's a node with children, it'll be bigger than the rest. Right now it shows everything with a child. Rather than the
+      // root nodes
+      radius: ({ children }) => (children.length > 0 ? 48 : 38),
+      type: 'circle',
+      color: ({ type }) => (type === 'Application' ? '#4466cc' : '#ADD8E6')
     },
     label: {
       visible: true,
       direction: 'south',
       directionAutoAdjustment: true,
-      text: getNodeLabel
+      margin: 4
+      // text: getNodeLabel
+    },
+    hover: {
+      type: 'circle',
+      radius: 40,
+      strokeWidth: 0,
+      strokeColor: '#000000',
+      strokeDasharray: '0',
+      // Checks for whether something is end of life, phaseOut, might wanna involve for the other phases as well. It's also only
+      // working for IT Components
+      color: ({ eol, phaseOut }) => (eol ? '#f87168' : phaseOut ? '#fea363' : 'grey')
     }
   },
   edge: {
     normal: {
+      width: 3,
+      dasharray: ({ type }) => (type === 'relToChild' ? 10 : 0),
       color: ({ type }) => {
         let color = 'red'
         switch (type) {
