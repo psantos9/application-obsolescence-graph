d4578dc8807f02748d3d7b18a034b79c9a7f146d
Jesus Bautista Lopez <jesusjblopez@gmail.com>
2023-11-27 08:52:36 -0500

    Adding changes

--- src/components/VGraph.vue
+++ src/components/VGraph.vue
@@ -1,34 +1,40 @@
 <template>
   <v-network-graph ref="graph" :nodes="nodes" :edges="edges" :configs="configs" :eventHandlers="eventHandlers">
-    <template #override-node-label="{ scale, text }">
-      <text x="0" y="0" :font-size="8 * scale" text-anchor="middle" dominant-baseline="central" fill="#ffffff">
+    <template #override-node-label="{ scale, text, config }">
+      <text
+        x="0"
+        y="0"
+        :font-size="config.fontSize * scale"
+        text-anchor="middle"
+        dominant-baseline="central"
+        fill="#ffffff">
         {{ text }}
       </text>
     </template>
     <template #edge-label="{ edge, ...slotProps }">
-      <v-edge-label :text="edge.obsolescenceRiskStatus" align="center" vertical-align="above" v-bind="slotProps" />
+      <v-edge-label
+        :text="edge.obsolescenceRiskStatus"
+        align="center"
+        vertical-align="verticalAlign"
+        v-bind="slotProps" />
     </template>
   </v-network-graph>
 </template>
 
+<style scoped></style>
+
 <script lang="ts" setup>
 import { ref, computed, unref } from 'vue'
 import { defineConfigs, type VNetworkGraphInstance, type LayoutHandler, type Node } from 'v-network-graph'
+import buildNetwork from 'v-network-graph'
 import { ForceLayout } from 'v-network-graph/lib/force-layout'
 import { useApi } from '@/composables/useApi'
-import dagre from '@dagrejs/dagre'
+import { AggregatedObsolescenceRisk } from '@/composables/leanix'
 
 const { graph: dataset, openFactSheetSidePane } = useApi()
 const layoutHandler: LayoutHandler = new ForceLayout()
 
 const graph = ref<VNetworkGraphInstance | null>(null)
-
-// Make references for tooltip, it's opacity, and the targetNode
-const tooltip = ref<HTMLDivElement>()
-const tooltipOpacity = ref(0)
-const targetNodeId = ref<string>('')
-const tooltipPos = ref({ left: '0px', top: '0px' })
-
 const nodes = computed(() => unref(dataset)?.nodes ?? {})
 const edges = computed(() => unref(dataset)?.edges ?? {})
 
@@ -62,11 +68,54 @@ const getNodeLabel = (node: Node): string => {
   return label.join('\n')
 }
 
-// For icons later on
+// Dynamically changes text
+const pickTextSize = (name: string, type: string): number => {
+  let text_size = 10
+  if (name.includes('Application')) {
+    let index = name.indexOf(' ')
+    for (index; index < name.length; index++) {
+      text_size -= 1
+    }
+  } else {
+    let index = name.indexOf(' ', 4)
+    for (index; index < name.length; index++) {
+      text_size -= 1
+    }
+
+    if (type == 'ITComponent') {
+      text_size -= 1
+    }
+  }
+
+  return text_size
+}
+// Dynamically changes size of children components
+const pickSize = (name: string, type: string): number => {
+  let radius_size = 60
+  if (name.includes('Application')) {
+    let index = name.indexOf(' ')
+    for (index; index < name.length; index++) {
+      radius_size -= 5
+    }
+  } else {
+    let index = name.indexOf(' ', 4)
+    for (index; index < name.length; index++) {
+      radius_size -= 5
+    }
+
+    if (type == 'ITComponent') {
+      radius_size -= 5
+    }
+  }
+
+  return radius_size
+}
+
+/* For icons later on
 const icons = {
   accepted: '&#xe5ca',
   addressed: '&#xe160'
-} as Record<string, string>
+} as Record<string, string> */
 
 const configs = defineConfigs({
   view: {
@@ -77,31 +126,33 @@ const configs = defineConfigs({
     normal: {
       // If it's a node with children, it'll be bigger than the rest. Right now it shows everything with a child. Rather than the
       // root nodes
-      radius: ({ children }) => (children.length > 0 ? 48 : 38),
-      type: 'circle',
+      radius: ({ name, type }) => (name && type ? pickSize(name, type) : 50),
+      type: ({ type }) => (type === 'ITComponent' ? 'circle' : 'rect'),
       color: ({ type }) => (type === 'Application' ? '#4466cc' : '#ADD8E6')
     },
     label: {
       visible: true,
       direction: 'south',
       directionAutoAdjustment: true,
-      margin: 4
-      // text: getNodeLabel
+      fontSize: ({ name, type }) => (name && type ? pickTextSize(name, type) : 8),
+      text: ({ name }) => (name ? name : '')
     },
+
     hover: {
       type: 'circle',
-      radius: 40,
+      radius: ({ name, type }) => (name && type ? pickSize(name, type) : 50),
       strokeWidth: 0,
       strokeColor: '#000000',
       strokeDasharray: '0',
       // Checks for whether something is end of life, phaseOut, might wanna involve for the other phases as well. It's also only
       // working for IT Components
-      color: ({ eol, phaseOut }) => (eol ? '#f87168' : phaseOut ? '#fea363' : 'grey')
+      color: ({ eol, phaseOut }) => (eol ? '#c00001' : phaseOut ? '#fcd42c' : '#4466cc')
     }
   },
   edge: {
     normal: {
       width: 3,
+      borderRadius: 2,
       dasharray: ({ type }) => (type === 'relToChild' ? 10 : 0),
       color: ({ type }) => {
         let color = 'red'
@@ -127,6 +178,11 @@ const configs = defineConfigs({
         width: 4,
         height: 4
       }
+    },
+    label: {
+      margin: 0,
+      fontSize: 11,
+      lineHeight: 1.1
     }
   }
 })
