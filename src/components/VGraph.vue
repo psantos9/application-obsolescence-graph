0b00b01a939cdba6ddee2f5cfd23eb9cc19ca241
Jesus Bautista Lopez <jesusjblopez@gmail.com>
2023-11-28 00:56:49 -0500

    Added visualization

--- src/components/VGraph.vue
+++ src/components/VGraph.vue
@@ -7,29 +7,61 @@
         :font-size="config.fontSize * scale"
         text-anchor="middle"
         dominant-baseline="central"
-        fill="#ffffff">
+        fill="white">
         {{ text }}
       </text>
     </template>
-    <template #edge-label="{ edge, ...slotProps }">
-      <v-edge-label
-        :text="edge.obsolescenceRiskStatus"
-        align="center"
-        vertical-align="verticalAlign"
-        v-bind="slotProps" />
+    <template #edge-overlay="{ edge, scale, length, pointAtLength }">
+      <g class="edge-icon">
+        <!-- length: The total length of the edge. -->
+        <circle
+          :cx="pointAtLength(length - 40 * scale).x"
+          :cy="pointAtLength(length - 40 * scale).y"
+          :r="
+            edge.obsolescenceRiskStatus === 'riskAddressed'
+              ? 10 * scale
+              : edge.obsolescenceRiskStatus === 'riskAccepted'
+              ? 10 * scale
+              : 0
+          "
+          stroke="#444"
+          :stroke-width="1 * scale"
+          :fill="
+            edge.obsolescenceRiskStatus === 'riskAddressed'
+              ? '#54c45e'
+              : edge.obsolescenceRiskStatus === 'riskAccepted'
+              ? '#ff80df'
+              : ''
+          " />
+        <text
+          v-bind="pointAtLength(length - 40 * scale)"
+          font-family="Material Icons"
+          text-anchor="middle"
+          dominant-baseline="central"
+          :font-size="16 * scale"
+          v-html="icons[edge.obsolescenceRiskStatus]" />
+      </g>
     </template>
   </v-network-graph>
 </template>
 
-<style scoped></style>
+<style scoped>
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/materialicons/v97/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2) format('woff2');
+}
+.edge-icon {
+  pointer-events: none;
+}
+</style>
 
 <script lang="ts" setup>
 import { ref, computed, unref } from 'vue'
 import { defineConfigs, type VNetworkGraphInstance, type LayoutHandler, type Node } from 'v-network-graph'
-import buildNetwork from 'v-network-graph'
 import { ForceLayout } from 'v-network-graph/lib/force-layout'
 import { useApi } from '@/composables/useApi'
-import { AggregatedObsolescenceRisk } from '@/composables/leanix'
 
 const { graph: dataset, openFactSheetSidePane } = useApi()
 const layoutHandler: LayoutHandler = new ForceLayout()
@@ -68,7 +100,37 @@ const getNodeLabel = (node: Node): string => {
   return label.join('\n')
 }
 
-// Dynamically changes text
+// Chooses a color based on the application or IT component
+const pickColor = (type: string, risk: string, phaseOut?: string, eol?: string): string => {
+  let color = ''
+  if (type == 'Application') {
+    if (risk == 'missingITComponent') {
+      color = '#8590a1'
+    } else if (risk == 'noRisk') {
+      color = '#baf3db'
+    } else if (risk == 'unaddressedEndOfLife') {
+      color = '#f87168'
+    } else if (risk == 'unaddressedPhaseOut') {
+      color = '#fea363'
+    } else if (risk == 'missingLifecycle') {
+      color = '#ededef'
+    }
+  } else {
+    if (eol) {
+      color = '#c00001'
+    } else if (phaseOut) {
+      color = '#fcd42c'
+    } else if (risk == 'phaseIn') {
+      color = '#e7f9ff'
+    } else if (risk == 'active') {
+      color = '#cce0ff'
+    } else if (risk == 'plan') {
+      color = '#f4f0ff'
+    }
+  }
+  return color
+}
+// Dynamically changes text, will not use this, I want all nodes to be of the same size
 const pickTextSize = (name: string, type: string): number => {
   let text_size = 10
   if (name.includes('Application')) {
@@ -111,11 +173,11 @@ const pickSize = (name: string, type: string): number => {
   return radius_size
 }
 
-/* For icons later on
+// Icons that'll display whether the risk was addressed or accepted
 const icons = {
-  accepted: '&#xe5ca',
-  addressed: '&#xe160'
-} as Record<string, string> */
+  riskAccepted: '&#xe86c',
+  riskAddressed: '&#xf091'
+} as Record<string, string>
 
 const configs = defineConfigs({
   view: {
@@ -124,47 +186,60 @@ const configs = defineConfigs({
   },
   node: {
     normal: {
-      // If it's a node with children, it'll be bigger than the rest. Right now it shows everything with a child. Rather than the
-      // root nodes
-      radius: ({ name, type }) => (name && type ? pickSize(name, type) : 50),
+      radius: 32,
+      // for type is "rect" -->
+      width: 64,
+      height: 64,
       type: ({ type }) => (type === 'ITComponent' ? 'circle' : 'rect'),
-      color: ({ type }) => (type === 'Application' ? '#4466cc' : '#ADD8E6')
+      color: ({ type, aggregatedObsolescenceRiskKey, aggregatedLifecycleKey, lifecycleKey, eol, phaseOut }) =>
+        aggregatedObsolescenceRiskKey
+          ? pickColor(type, aggregatedObsolescenceRiskKey, eol, phaseOut)
+          : aggregatedLifecycleKey
+          ? pickColor(type, aggregatedLifecycleKey, eol, phaseOut)
+          : pickColor(type, lifecycleKey, eol, phaseOut)
     },
     label: {
       visible: true,
       direction: 'south',
       directionAutoAdjustment: true,
-      fontSize: ({ name, type }) => (name && type ? pickTextSize(name, type) : 8),
+      fontSize: 7,
       text: ({ name }) => (name ? name : '')
     },
 
     hover: {
-      type: 'circle',
-      radius: ({ name, type }) => (name && type ? pickSize(name, type) : 50),
+      type: ({ type }) => (type === 'ITComponent' ? 'circle' : 'rect'),
+      radius: 32,
+      // for type is "rect" -->
+      width: 64,
+      height: 64,
       strokeWidth: 0,
       strokeColor: '#000000',
       strokeDasharray: '0',
-      // Checks for whether something is end of life, phaseOut, might wanna involve for the other phases as well. It's also only
-      // working for IT Components
-      color: ({ eol, phaseOut }) => (eol ? '#c00001' : phaseOut ? '#fcd42c' : '#4466cc')
+      color: ({ type, aggregatedObsolescenceRiskKey, aggregatedLifecycleKey, lifecycleKey, eol, phaseOut }) =>
+        aggregatedObsolescenceRiskKey
+          ? pickColor(type, aggregatedObsolescenceRiskKey, eol, phaseOut)
+          : aggregatedLifecycleKey
+          ? pickColor(type, aggregatedLifecycleKey, eol, phaseOut)
+          : pickColor(type, lifecycleKey, eol, phaseOut)
     }
   },
   edge: {
     normal: {
-      width: 3,
+      gap: 10,
+      width: ({ type }) => (type === 'relToRequires' ? 1 : 3),
       borderRadius: 2,
-      dasharray: ({ type }) => (type === 'relToChild' ? 10 : 0),
+      dasharray: ({ type }) => (type === 'relToChild' ? '4' : '0'),
       color: ({ type }) => {
-        let color = 'red'
+        let color = ''
         switch (type) {
           case 'relToChild':
-            color = 'blue'
+            color = '#1171e5'
             break
           case 'relToRequires':
-            color = 'red'
+            color = '#fcce15'
             break
           case 'relApplicationToITComponent':
-            color = 'green'
+            color = '#e71413'
             break
           default:
             color = 'gray'
@@ -176,13 +251,31 @@ const configs = defineConfigs({
       target: {
         type: 'arrow',
         width: 4,
-        height: 4
+        height: 4,
+        margin: -1
       }
     },
-    label: {
-      margin: 0,
-      fontSize: 11,
-      lineHeight: 1.1
+    hover: {
+      width: 4,
+      borderRadius: 2,
+      dasharray: ({ type }) => (type === 'relToChild' ? '4' : '0'),
+      color: ({ type }) => {
+        let color = ''
+        switch (type) {
+          case 'relToChild':
+            color = '#1171e5'
+            break
+          case 'relToRequires':
+            color = '#fcce15'
+            break
+          case 'relApplicationToITComponent':
+            color = '#e71413'
+            break
+          default:
+            color = 'gray'
+        }
+        return color
+      }
     }
   }
 })
