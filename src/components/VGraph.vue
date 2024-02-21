e5621b27eca3cb7a795c887521a4b96b32d6e00d
Jesus Bautista Lopez <jesusjblopez@gmail.com>
2024-02-20 20:10:13 -0500

    Added constant size for nodes

--- src/components/VGraph.vue
+++ src/components/VGraph.vue
@@ -58,17 +58,44 @@
 </style>
 
 <script lang="ts" setup>
-import { ref, computed, unref } from 'vue'
-import { defineConfigs, type VNetworkGraphInstance, type LayoutHandler, type Node } from 'v-network-graph'
-import { ForceLayout } from 'v-network-graph/lib/force-layout'
+import { ref, computed, unref, watch } from 'vue'
+import {
+  defineConfigs,
+  type VNetworkGraphInstance,
+  type LayoutHandler,
+  type Nodes,
+  type Edges,
+  type Node
+} from 'v-network-graph'
+import { ForceLayout, type ForceNodeDatum, type ForceEdgeDatum } from 'v-network-graph/lib/force-layout'
 import { useApi } from '@/composables/useApi'
 
 const { graph: dataset, openFactSheetSidePane } = useApi()
-const layoutHandler: LayoutHandler = new ForceLayout()
+const layoutHandler: LayoutHandler = new ForceLayout({
+  positionFixedByDrag: false,
+  positionFixedByClickWithAltKey: true,
+  createSimulation: (d3, nodes, edges) => {
+    // d3-force parameters
+    const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id((d) => d.id)
+    return d3
+      .forceSimulation(nodes)
+      .force('edge', forceLink.distance(40).strength(0.5))
+      .force('charge', d3.forceManyBody().strength(-800))
+      .force('center', d3.forceCenter().strength(0.05))
+      .alphaMin(0.001)
+  }
+})
 
 const graph = ref<VNetworkGraphInstance | null>(null)
 const nodes = computed(() => unref(dataset)?.nodes ?? {})
 const edges = computed(() => unref(dataset)?.edges ?? {})
+const nodeLength = computed(() => Object.keys(unref(nodes)).length)
+
+buildNetwork(20, nodes.value, edges.value)
+
+watch(20, () => {
+  buildNetwork(20, nodes.value, edges.value)
+})
 
 const getNodeLabel = (node: Node): string => {
   const {
@@ -101,7 +128,7 @@ const getNodeLabel = (node: Node): string => {
 }
 
 // Chooses a color based on the application or IT component
-const pickColor = (type: string, risk: string, phaseOut?: string, eol?: string): string => {
+const pickColor = (type: string, risk: string, phaseOut: string, eol: string): string => {
   let color = ''
   if (type == 'Application') {
     if (risk == 'missingITComponent') {
@@ -116,10 +143,10 @@ const pickColor = (type: string, risk: string, phaseOut?: string, eol?: string):
       color = '#ededef'
     }
   } else {
-    if (eol) {
-      color = '#c00001'
-    } else if (phaseOut) {
+    if (phaseOut) {
       color = '#fcd42c'
+    } else if (eol) {
+      color = '#c00001'
     } else if (risk == 'phaseIn') {
       color = '#e7f9ff'
     } else if (risk == 'active') {
@@ -182,6 +209,10 @@ const icons = {
 const configs = defineConfigs({
   view: {
     autoPanAndZoomOnLoad: 'fit-content',
+    scalingObjects: true,
+    // Added constant size to nodes
+    minZoomLevel: 0.5,
+    maxZoomLevel: 1,
     layoutHandler
   },
   node: {
@@ -193,10 +224,10 @@ const configs = defineConfigs({
       type: ({ type }) => (type === 'ITComponent' ? 'circle' : 'rect'),
       color: ({ type, aggregatedObsolescenceRiskKey, aggregatedLifecycleKey, lifecycleKey, eol, phaseOut }) =>
         aggregatedObsolescenceRiskKey
-          ? pickColor(type, aggregatedObsolescenceRiskKey, eol, phaseOut)
+          ? pickColor(type, aggregatedObsolescenceRiskKey, phaseOut, eol)
           : aggregatedLifecycleKey
-          ? pickColor(type, aggregatedLifecycleKey, eol, phaseOut)
-          : pickColor(type, lifecycleKey, eol, phaseOut)
+          ? pickColor(type, aggregatedLifecycleKey, phaseOut, eol)
+          : pickColor(type, lifecycleKey, phaseOut, eol)
     },
     label: {
       visible: true,
@@ -217,17 +248,17 @@ const configs = defineConfigs({
       strokeDasharray: '0',
       color: ({ type, aggregatedObsolescenceRiskKey, aggregatedLifecycleKey, lifecycleKey, eol, phaseOut }) =>
         aggregatedObsolescenceRiskKey
-          ? pickColor(type, aggregatedObsolescenceRiskKey, eol, phaseOut)
+          ? pickColor(type, aggregatedObsolescenceRiskKey, phaseOut, eol)
           : aggregatedLifecycleKey
-          ? pickColor(type, aggregatedLifecycleKey, eol, phaseOut)
-          : pickColor(type, lifecycleKey, eol, phaseOut)
+          ? pickColor(type, aggregatedLifecycleKey, phaseOut, eol)
+          : pickColor(type, lifecycleKey, phaseOut, eol)
     }
   },
   edge: {
     normal: {
       gap: 10,
       width: ({ type }) => (type === 'relToRequires' ? 1 : 3),
-      borderRadius: 2,
+      borderRadius: 1,
       dasharray: ({ type }) => (type === 'relToChild' ? '4' : '0'),
       color: ({ type }) => {
         let color = ''
@@ -250,8 +281,8 @@ const configs = defineConfigs({
     marker: {
       target: {
         type: 'arrow',
-        width: 4,
-        height: 4,
+        width: 3,
+        height: 3,
         margin: -1
       }
     },
@@ -279,6 +310,43 @@ const configs = defineConfigs({
     }
   }
 })
+
+// Legend for chart
+lx.showLegend([
+  { label: 'Risk Addressed', bgColor: '#54c45e' },
+  { label: 'Risk Accepted', bgColor: '#ff80df' },
+  { label: 'Missing IT Component', bgColor: '#8590a1' },
+  { label: 'No Risk', bgColor: '#baf3db' },
+  { label: 'Unaddressed End of Life', bgColor: '#f87168' },
+  { label: 'Unaddressed Phase Out', bgColor: '#fea363' },
+  { label: 'Phase Out', bgColor: '#fcd42c' },
+  { label: 'Phase In', bgColor: '#e7f9ff' },
+  { label: 'Active', bgColor: '#cce0ff' },
+  { label: 'Plan', bgColor: '#f4f0ff' },
+  { label: 'End of Life', bgColor: '#c00001' }
+])
+
+function buildNetwork(count: number, nodes: Nodes, edges: Edges) {
+  const idNums = [...Array(count)].map((_, i) => i)
+
+  // nodes
+  const newNodes = Object.fromEntries(idNums.map((id) => [`node${id}`, {}]))
+  Object.keys(nodes).forEach((id) => delete nodes[id])
+  Object.assign(nodes, newNodes)
+
+  // edges
+  const makeEdgeEntry = (id1: number, id2: number) => {
+    return [`edge${id1}-${id2}`, { source: `node${id1}`, target: `node${id2}` }]
+  }
+  const newEdges = Object.fromEntries([
+    ...idNums
+      .map((n) => [n, (Math.floor(n / 4) * 4) % count])
+      .map(([n, m]) => (n === m ? [n, (n + 4) % count] : [n, m]))
+      .map(([n, m]) => makeEdgeEntry(n, m))
+  ])
+  Object.keys(edges).forEach((id) => delete edges[id])
+  Object.assign(edges, newEdges)
+}
 
 const eventHandlers = {
   'node:click': openFactSheetSidePane
